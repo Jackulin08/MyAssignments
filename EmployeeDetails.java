01a1779f3367935c1b50c5acf86d28785e990cb6
Jackulin08 <jackulin.sujitha28@gmail.com>
2026-02-01 04:49:30 +0000

    Add files via upload
    First Assignment

---
+++ EmployeeDetails.java
@@ -0,0 +1,39 @@
+package assgn1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name="John Doe";
+		int age=30;
+		float salary=55000.75f;
+		char gender='M';
+		boolean acceptTerms=true;
+		float rating=4.5f;
+		long mobileNumber=9876543210L;
+		int pincode=56001;
+		char luckyNumber='7';
+		
+		//System.out.println(name);
+		//System.out.println(Age);
+		System.out.println("Name:"+name+"\n"+"Age:"+age);
+		//System.out.println(salary);
+		//System.out.println(gender);
+		System.out.println("Salary:"+salary+"\n"+"Gender:"+gender);
+		 //System.out.println(acceptTerms);
+		//System.out.println(rating);
+		//System.out.println(mobileNumber);
+		System.out.println("AcceptTerms:"+acceptTerms+"\n"+"Rating:"+rating+"\n"+"MobileNumber:"+mobileNumber);
+		//System.out.println(pincode);
+		//System.out.println(luckyNumber);
+		System.out.println("Pincode:"+pincode+"\n"+"LuckyNumber:"+luckyNumber);
+		
+        
+		
+		
+		
+		
+	}
+	
+}
